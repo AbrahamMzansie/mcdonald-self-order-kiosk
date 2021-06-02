704b997f24a219149fbb4014a3fb24311ae33153
unknown <abrahaam.nkomo@gmail.com>
2021-06-03 01:12:09 +0200

    added products functionality

--- data.js
+++ data.js
@@ -1,17 +1,93 @@
 const data = {
-    categories : [
-        {
-            name : "Beverages" ,
-            image : "/images/beverages.jpg"
-        },
-        {
-            name : "Breakfast" ,
-            image : "/images/breakfast.jpg"
-        },
-        {
-            name : "Burgers" ,
-            image : "/images/burgers.jpg"
-        }
-    ]
+  categories: [
+    {
+      name: "Beverages",
+      image: "/images/beverages.jpg",
+    },
+    {
+      name: "Breakfast",
+      image: "/images/breakfast.jpg",
+    },
+    {
+      name: "Burgers",
+      image: "/images/burgers.jpg",
+    },
+  ],
+  products: [
+    {
+      name: "Coca-Cola",
+      description: "Coca-Cola",
+      image: "/images/t-mcdonalds-Coca-Cola-Classic-Small.jpg",
+      price: 10,
+      category: "Beverages",
+      calories: 120,
+    },
+    {
+      name: "Vanilla Shake",
+      description: "Vanilla Shake",
+      image: "/images/t-mcdonalds-Vanilla-McCafe-Shake-Medium.jpg",
+      price: 15,
+      category: "Beverages",
+      calories: 330,
+    },
+    {
+      name: "Hot Chocolate",
+      description: "Hot Chocolate",
+      image: "/images/t-mcdonalds-McCafe-Hot-Chocolate-Medium.jpg",
+      price: 11,
+      category: "Beverages",
+      calories: 320,
+    },
+    {
+      name: "Bacon and Biscuits",
+      description: "Bacon and Biscuits",
+      image:
+        "/images/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit.jpg",
+      price: 10.9,
+      category: "Breakfast",
+      calories: 120,
+    },
+    {
+      name: "Blueberry Muffin",
+      description: "Blueberry Muffin",
+      image: "/images/t-blueberry-muffin.jpg",
+      price: 18.9,
+      category: "Breakfast",
+      calories: 500,
+    },
+    {
+      name: "Big Breakfast",
+      description: "Big Breakfast",
+      image: "/images/s-mcdonalds-Big-Breakfast-Regular-Size-Biscuit.jpg",
+      price: 15.9,
+      category: "Breakfast",
+      calories: 520,
+    },
+
+    {
+      name: "Big Mac",
+      description: "Big Mac",
+      image: "/images/t-mcdonalds-Big-Mac.jpg",
+      price: 70.9,
+      category: "Burgers",
+      calories: 70,
+    },
+    {
+      name: "Hamburger",
+      description: "Hamburger",
+      image: "/images/t-mcdonalds-Hamburger.jpg",
+      price: 50.9,
+      category: "Burgers",
+      calories: 20,
+    },
+    {
+      name: "McDouble",
+      description: "McDouble",
+      image: "/images/t-mcdonalds-McDouble.jpg",
+      price: 40.9,
+      category: "Burgers",
+      calories: 200,
+    },
+  ],
 };
 module.exports = data;
--- server.js
+++ server.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const data = require("./data");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const Products = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    price: Number,
+    category: String,
+    calories: Number,
+  })
+);
+app.get("/api/products/seeds", async (req, res) => {
+  const products = await Products.insertMany(data.products);
+  res.send({ products });
+});
+
+app.get("/api/products", async (req, res) => {
+  const { category } = req.query;
+  const products = await Products.find(category ? { category } : {});
+  res.send({ products });
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = newProduct.save();
+  res.send({ savedProduct });
+});
 
 app.get("/api/categories", (req, res) => {
   res.send(data.categories);
